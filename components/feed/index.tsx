f083f30f8cc4cdd82b52dfb9e177082bc8e2550f
Alan Campora <alan.campora@gmail.com>
2020-12-07 21:33:27 -0300

    feat(header): modify styles

--- components/feed/index.tsx
+++ components/feed/index.tsx
@@ -18,37 +18,65 @@ export function Feed({ onLogout, onUpload }: Props) {
   if (loading) return <div>Loading Feed</div>;
   if (error) return <div>"There was an error while loading feed"</div>;
   return (
-    <div>
-      <header className="text-gray-700 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <div className="bg-gray-50">
+      <header className="bg-white text-gray-700 body-font fixed w-full z-1 top-0 left-0 shadow-sm">
+        <div className="flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <img className="w-16 h-16 rounded-full" src={user?.photoURL}></img>
-            <span className="ml-3 text-xl">{user?.displayName}</span>
+            <span className="ml-3 text-xl">Moments</span>
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <button
-              onClick={onLogout}
-              className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0"
-            >
-              Logout
+            <img className="mr-4 w-8 h-8 rounded-full" src={user?.photoURL}></img>
+            <button className="mr-4" onClick={onUpload}>
+              <svg
+                className="w-8"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                />
+              </svg>
             </button>
-            <button
-              onClick={onUpload}
-              className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0"
-            >
-              Upload Image
+
+            <button className="mr-4" onClick={onLogout}>
+              <svg
+                className="w-8"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
             </button>
           </nav>
         </div>
       </header>
 
-      <section className="text-gray-700 body-font max-w-614 mx-auto">
+      <div id="separator" className="py-4"></div>
+
+      <section className="text-gray-700 body-font max-w-614 container mx-auto max-width-614px">
         <div className="flex flex-col items-center">
           {snapshots.map((v, index) => {
             const { username, avatar, description, image } = v.val();
             return (
               <div key={index}>
-                <Card description={description} image={image} avatar={avatar} username={username}/>
+                <Card
+                  description={description}
+                  image={image}
+                  avatar={avatar}
+                  username={username}
+                />
               </div>
             );
           })}
--- components/upload/index.tsx
+++ components/upload/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import { Modal } from "../modal";
 import { UserContext, User } from "../../providers";
 import firebase from "firebase";
@@ -29,20 +29,25 @@ const onUpload = (
     },
     (error) => console.log({ error }),
     async () => {
-      const downloadURL = await task.snapshot.ref.getDownloadURL();
-      const record = {
-        avatar: user.photoURL,
-        username: user.displayName,
-        image: downloadURL,
-        description: description,
-      };
+      try {
+        const downloadURL = await task.snapshot.ref.getDownloadURL();
+        const record = {
+          avatar: user.photoURL,
+          username: user.displayName,
+          image: downloadURL,
+          description: description,
+        };
 
-      const db = firebase.database();
-      const dbRef = db.ref("uploads");
+        const db = firebase.database();
+        const dbRef = db.ref("uploads");
 
-      const newUpload = dbRef.push();
-      newUpload.set(record);
-      onSuccess();
+        const newUpload = dbRef.push();
+        newUpload.set(record);
+      } catch (e) {
+        console.log(`error: ${error}`);
+      } finally {
+        onSuccess();
+      }
     }
   );
 };
@@ -51,6 +56,7 @@ export function Upload({ show, onClose }: Props) {
   const { user } = useContext(UserContext);
   const [description, setDescription] = useState<string>("");
   const [file, setFile] = useState<File>();
+  const fileRef = useRef();
   return (
     user && (
       <Modal
@@ -59,7 +65,13 @@ export function Upload({ show, onClose }: Props) {
         actions={[
           {
             label: "Upload",
-            onClick: () => onUpload({ user, description, file }, onClose),
+            onClick: () =>
+              onUpload({ user, description, file }, () => {
+                setDescription("");
+                console.log({fileRef})
+                setFile(null);
+                onClose();
+              }),
           },
           {
             label: "Close",
@@ -82,6 +94,7 @@ export function Upload({ show, onClose }: Props) {
             <div className="p-2">
               <label htmlFor="file">Upload: </label>
               <input
+                ref={fileRef}
                 id="file"
                 type="file"
                 onChange={(e) => {
